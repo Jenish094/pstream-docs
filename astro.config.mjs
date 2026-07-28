403c7f503feb158a985990b1b36fcafff4464efe
Jenish Pathak <46525717+Jenish094@users.noreply.github.com>
2026-07-28 13:00:36 +0930

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import Icons from 'unplugin-icons/vite';
 
 // Allow site URL and base path to be overridden via environment variables
 const site = process.env.SITE_URL ?? 'https://docs.pstream.jenish.tech';
-const base = process.env.BASE_PATH ?? '/';
+const base = process.env.BASE_PATH ?? '/pstream-docs';
 
 export default defineConfig({
   site,
